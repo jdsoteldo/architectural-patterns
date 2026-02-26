3cb1cc5af3abd5ca1359fafcd4d4dfa482696ecf
JD Soteldo <jdsoteldo00@gmail.com>
2026-02-26 12:25:12 -0500

    setup gemfile

---
+++ layered_bookstore/presentation/cli.rb
@@ -0,0 +1,8 @@
+# features list:
+# main menu with options:
+# 1. list of books -> show all book -> recommend a book -> exit
+# show the book -> order it -> anything else -> exit
+# order -> ask for customer details, name, address, email address -> (simulating a stripe modal) ask for payment details -> confirms the order
+# once order is confirmed -> updates the db following the layers (never directly from view)
+# search? 
+# 9. exit
